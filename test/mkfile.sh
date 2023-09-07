ac28e8a15ca2b5ce07b589472d8ef298978e0425
Andy <84570361+Godovsky@users.noreply.github.com>
2023-09-07 19:05:07 +0300

    mkfile.sh

---
+++ test/mkfile.sh
@@ -0,0 +1,8 @@
+cd ./src
+gcc -Wall -c main.c
+read -p "Press enter to continue"
+gcc -Wall -o ../execute main.o
+read -p "Press enter to continue"
+cd ..
+./execute
+read -p "Press enter to continue"
